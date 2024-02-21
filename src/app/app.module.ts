d0e2be0bac6ce6678dc921cb4ffc5e4f3cbcde3f
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2024-02-22 01:27:48 +0530

    skeleton setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { SigninComponent } from './pages/signin/signin.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { RateUserComponent } from './pages/rate-user/rate-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    SigninComponent,
+    DashboardComponent,
+    RateUserComponent
   ],
   imports: [
     BrowserModule,
