2101d81a471087ca95cc3d933c49253a522a7e35
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2024-02-26 15:38:38 +0530

    rate page

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,19 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { RateMeNowService } from 'src/app/services/rate-me-now.service';
+import { RootScopeService } from 'src/app/services/root-scope.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
   isprofileButtonClicked: boolean = false;
-  constructor() { }
+  documentClickHandler: any;
+
+  constructor(private rateMeNowService: RateMeNowService, private router: Router, public rootScopeService: RootScopeService, private elementRef: ElementRef) {
+    this.documentClickHandler = this.handleDocumentClick.bind(this);
+  }
+
   user: string = "";
   ngOnInit(): void {
+    this.documentClickHandler = this.handleDocumentClick.bind(this);
+    document.addEventListener('click', this.documentClickHandler);
+  }
+
+  ngOnDestroy(): void {
+    document.removeEventListener('click', this.documentClickHandler);
   }
 
   profilButtonClicked() {
     this.isprofileButtonClicked = !this.isprofileButtonClicked;
+  }
+
+  private handleDocumentClick(event: MouseEvent): void {
+    if (!this.elementRef.nativeElement.contains(event.target)) {
+      this.isprofileButtonClicked = false;
+    }
   }
 
 }
--- src/app/pages/rate-user/rate-user.component.ts
+++ src/app/pages/rate-user/rate-user.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-rate-user',
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RateUserComponent implements OnInit {
 
+  @Input() userDetails: any;
+  @Output() backbuttonClicked = new EventEmitter<any>();
+
   constructor() { }
 
   ngOnInit(): void {
+    
+
+  }
+
+  backClicked() {
+    this.backbuttonClicked.emit();
   }
 
 }
